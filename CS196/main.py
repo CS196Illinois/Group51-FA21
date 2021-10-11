f68e28da51d3c46cc3dd0f1eb91db89cbaaefe39
Aryan-V <45548390+Aryan-V@users.noreply.github.com>
2021-10-11 17:06:54 -0500

    initial commit

---
+++ CS196/main.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+import numpy as np
+import soundfile as sf
+import pydub
+
+import librosa.display
+
+
+# convert m4a file to .wav
+sound = pydub.AudioSegment.from_file("input/OjuSpeaking.m4a")
+sound.export("input/OjuSpeaking.wav", format="wav")
+
+# audio is a series of floating points
+audio, sampling_rate = librosa.load('input/OjuSpeaking.wav', duration=120)
+
+# stft turns the audio into a spectogram, and magphase outputs the magnitude and phase of the spectogram
+full_spectogram, phase = librosa.magphase(librosa.stft(audio))
+
+# each spectogram column is replaced by a combination of its nearest neighbors, which reduces noise
+spectogram_filter = librosa.decompose.nn_filter(full_spectogram,
+                                                aggregate=np.median,
+                                                metric='cosine',
+                                                width=int(librosa.time_to_frames(2, sr=sampling_rate)))
+
+# to ensure the output isn't greater than the input, so taking the minimum of the reduced noise spectogram and regular spectogram ensures that
+spectogram_filter = np.minimum(full_spectogram, spectogram_filter)
+
+# constants for the softmasks
+margin_back, margin_fore = 2, 10
+power = 2
+
+# acts as a mask over the spectogram that permits only a certain level of transparency
+mask_back = librosa.util.softmask(spectogram_filter,
+                               margin_back * (full_spectogram - spectogram_filter),
+                               power=power)
+
+mask_fore = librosa.util.softmask(full_spectogram - spectogram_filter,
+                               margin_fore * spectogram_filter,
+                               power=power)
+
+# creates spectograms of the foreground and background
+foreground_spectogram = mask_fore * full_spectogram
+background_spectogram = mask_back * full_spectogram
+
+# creates a digital spectogram variable of the foreground
+digital_foreground = foreground_spectogram * phase
+
+# transforms the digital spectogram to audio file
+audio_foreground = librosa.istft(digital_foreground)
+
+# writes to the given file path with the audio file at the given sampling rate
+sf.write('output/OjuSpeakingForeground.wav', audio_foreground, sampling_rate)
