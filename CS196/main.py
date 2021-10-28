933f8a08a7a9847b3773196c52b098e35d148d66
Aryan-V <45548390+Aryan-V@users.noreply.github.com>
2021-10-27 21:24:06 -0500

    modified main.py to work for any m4a in input folder without .wav already created

--- CS196/main.py
+++ CS196/main.py
@@ -10,22 +10,24 @@ import librosa
 import IPython
 
 
-input_direc = "OjuSpeaking"
+input_dir = "OjuSpeaking"
 
 # convert m4a file to .wav
-sound = pydub.AudioSegment.from_file("input/" + input_direc + ".m4a")
-sound.export("input/" + input_direc + ".wav", format="wav")
+sound = pydub.AudioSegment.from_file("input/" + input_dir + ".m4a")
+sound.export("input/" + input_dir + ".wav", format="wav")
 
 # audio is a series of floating points
-audio, sampling_rate = librosa.load("input/" + input_direc + ".wav", duration=120)
+audio, sampling_rate = librosa.load("input/" + input_dir + ".wav", duration=120)
+
+background_mask_librosa.backgroundOutput(audio, sampling_rate, input_dir)
 
 # input original speaking audio, retrieve data and rate of recording
-local_wave = "input/" + input_direc + ".wav"
+local_wave = "input/" + input_dir + ".wav"
 rate, data = wavfile.read(local_wave)
 data = data / 32768
 
 # input solely background noise from background_mask_librosa.py, retrieve data and rate of recording
-noise = "output/" + input_direc + "Background.wav"
+noise = "output/" + input_dir + "Background.wav"
 rateN, dataN = wavfile.read(noise)
 dataN = dataN / 32768
 
@@ -33,13 +35,11 @@ dataN = dataN / 32768
 noise_clip = dataN
 audio_clip_band_limited = data
 
-background_mask_librosa.backgroundOutput(audio, sampling_rate, input_direc)
-
 output = spectral_gating.removeNoise(audio_clip=audio_clip_band_limited, noise_clip=noise_clip)
 
 cleaned_audio = IPython.display.Audio(data=output, rate=44100)
 
-with open("output/" + input_direc + "BackgroundRemoved.wav", 'wb') as f:
+with open("output/" + input_dir + "BackgroundRemoved.wav", 'wb') as f:
     f.write(cleaned_audio.data)
 
-wav_to_csv.wav2csv("output/" + input_direc + "BackgroundRemoved.wav")
+wav_to_csv.wav2csv("output/" + input_dir + "BackgroundRemoved.wav")
