597448a8756c8a1b6a71cd46db20c4f3cb0f7387
Aryan-V <45548390+Aryan-V@users.noreply.github.com>
2021-11-01 22:28:07 -0500

    XCWorkspace Created with Icon set

---
+++ Clarify XCode Workspace/Clarify XCode Workspace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clarify XCode Workspace
+//
+//  Created by Aryan Vaswani on 11/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
