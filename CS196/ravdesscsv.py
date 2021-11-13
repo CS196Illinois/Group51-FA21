c4ba69a796e33aa752be41bd31c36ada6c2ea8d2
anaghab3 <90574783+anaghab3@users.noreply.github.com>
2021-11-13 11:12:05 -0600

    added RNN -- still need to alter some aspects but changed nn class to rnn class

--- CS196/ravdesscsv.py
+++ CS196/ravdesscsv.py
@@ -44,7 +44,7 @@ train_dataset, test_dataset = torch.utils.data.random_split(train, [train_size, 
 
 train_loader = data_utils.DataLoader(train_dataset, batch_size=16, shuffle=True)
 
-class Network(nn.Module):
+"""class Network(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -65,8 +65,29 @@ class Network(nn.Module):
         x = self.softmax(x)
 
         return x
+"""
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
 
+        self.hidden_size = hidden_size
 
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+n_hidden = 128
+Network = RNN(20, n_hidden, CLASSES)
 model = Network().to(device)
 print(model)
 
@@ -153,3 +174,25 @@ with torch.no_grad():
 
     # Print accuracy
     print('Accuracy: %d %%' % (100 * correct / total))
+
+"""
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+"""
