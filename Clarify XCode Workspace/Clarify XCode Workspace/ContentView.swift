8603d58f6b92d812371ea61357058af3ac97dc1a
ojaswee2 <ojaswee2@illinois.edu>
2021-11-13 12:09:31 -0600

    added an icon, added contentview file...may delete later

---
+++ Clarify XCode Workspace/Clarify XCode Workspace/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Clarify XCode Workspace
+//
+//  Created by Ojaswee Chaudhary on 11/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        Image("AppIcon");
+        
+    }
+}
+
+struct ContentView_Previews:
+                                
+    PreviewProvider {
+    
+        static var previews : some
+        View {
+            
+            ContentView()
+        }
+    }
+
--- Clarify XCode Workspace/Clarify XCode Workspace/ViewController.swift
+++ Clarify XCode Workspace/Clarify XCode Workspace/ViewController.swift
@@ -19,7 +19,5 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-
 }
 
